86a26a808a5283f64aa0c7890b48b48b7cbe2758
heronet <sirat4757@gmail.com>
2022-12-30 03:41:58 +0600

    167

---
+++ medium/167-two-sum-ii-input-array-is-sorted.rs
@@ -0,0 +1,21 @@
+struct Solution;
+
+impl Solution {
+    pub fn two_sum(numbers: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut left = 0;
+        let mut right = numbers.len() - 1;
+        
+        while left < right {
+            let sum = numbers[left] + numbers[right];
+            if sum > target {
+                right -= 1;
+            } else if sum < target {
+                left += 1;
+            }
+            else {
+                return vec![left as i32 + 1, right as i32 + 1];
+            }
+        }
+        return vec![];
+    }
+}
